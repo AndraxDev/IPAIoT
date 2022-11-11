14e10e53f2fed0bfde4a916992e93812f5bfb21e
AndraxDev <dostapenko82@gmail.com>
2022-11-11 12:51:27 +0100

    webform

--- webform.php
+++ webform.php
@@ -43,22 +43,157 @@ $app = <<<EOL
     <head>
         <title>Registration</title>
         <meta charset = "utf-8">
+        <style>
+            * {
+                font-family: sans-serif;
+            }
+
+            .content {
+                width: fit-content;
+            }
+
+            .mtrl-form {
+                width: fit-content;
+            }
+
+            .req {
+                color: red;
+            }
+
+            .req-field:invalid {
+                background-color: rgba(255, 220, 220, 0.2);
+                border: 2px solid red;
+                color: red;
+            }
+
+            .req-field {
+                border: 2px solid green;
+                color: green;
+            }
+
+            .mtrl-field {
+                outline: none;
+                border: 2px solid #255d8b;
+                border-radius: 8px;
+                padding: 4px;
+                height: 24px;
+                margin: 4px;
+                min-width: 200px;
+                background-color: rgba(198, 228, 255, 0.2);
+            }
+
+            label {
+                margin-left: 4px;
+                margin-right: 4px;
+                font-size: 14pt;
+            }
+
+            .mtrl-field:focus {
+                outline: none;
+                border: 2px solid #000;
+            }
+
+            .form-title {
+                color: #255d8b;
+                font-size: 16pt;
+            }
+
+            .form-border {
+                border: 2px solid #255d8b;
+                border-radius: 16px;
+                margin-top: 8px;
+                margin-bottom: 16px;
+                background-color: rgba(198, 228, 255, 0.5);
+                backdrop-filter: blur(256px);
+                    -webkit-backdrop-filter: blur(256px);
+            }
+
+            .textarea-field {
+                min-width: 700px;
+                min-height: 450px;
+                width: calc(100% - 20px);
+                resize: vertical;
+            }
+
+            .form-checks {
+                color: #255d8b;
+                cursor: pointer;
+            }
+
+            .mtrl-button-dark {
+                border-radius: 48px;
+                height: 48px;
+                border: none;
+                background-color: #255d8b;
+                color: #ffffff;
+                padding: 0 24px;
+                margin: 0;
+                cursor: pointer;
+                transition: 0.3s;
+                box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+            }
+
+            .mtrl-button-dark:hover {
+                background-color: #255d8b;
+                cursor: pointer;
+                transition: 0.3s;
+                box-shadow: 0 4px 4px rgba(0, 0, 0, 0.2);
+            }
+
+            .mtrl-button-outline-dark {
+                vertical-align: middle;
+                display: table-cell;
+                border-radius: 48px;
+                height: 48px;
+                background-color: rgba(0, 0, 0, 0);
+                border: 1px solid #255d8b;
+                color: #255d8b;
+                padding: 0 22px;
+                margin: 0 12px;
+                cursor: pointer;
+                transition: 0.3s;
+                box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+            }
+
+            .mtrl-button-outline-dark:hover {
+                border: 1px solid #255d8b;
+                color: #255d8b;
+                cursor: pointer;
+                transition: 0.3s;
+                box-shadow: 0 4px 4px rgba(0, 0, 0, 0.2);
+            }
+        </style>
     </head>
     <body>
-        <form action = "" method = "POST">
-            <input name = "name" id = "name" placeholder = "First name">
-            <br>
-            <input name = "surname" id = "surname" placeholder = "Last name">
-            <br>
-            <input name = "email" id = "email" placeholder = "Email">
-            <br>
-            <input name = "age" id = "age" type = "number" placeholder = "Age">
-            <br>
-            <input name = "country" id = "country" placeholder = "Country">
-            <br>
-            <textarea name = "bio" id = "bio" placeholder = "Bio"></textarea>
-            <input type = "submit" value = "Register">
-        </form>
+        <div class = "content">
+            <form action = "" method = "POST">
+                <fieldset class = "form-border">
+                    <legend class = "form-title">Registration</legend>
+                    <label for = "name">Name<span class = "req">*</span></label>
+                    <input name = "name" id = "name" class = "mtrl-field req-field" required>
+                    <br>
+                    <label for = "surname">Surname<span class = "req">*</span></label>
+                    <input name = "surname" id = "surname" class = "mtrl-field req-field" required>
+                    <br>
+                    <label for = "email">Email<span class = "req">*</span></label>
+                    <input name = "email" type = "email" id = "email" class = "mtrl-field req-field" required>
+                    <br>
+                    <label for = "age">Age<span class = "req">*</span></label>
+                    <input name = "age" id = "age" type = "number" class = "mtrl-field req-field" required>
+                    <br>
+                    <label for = "country">Country<span class = "req">*</span></label>
+                    <input name = "country" id = "country" class = "mtrl-field req-field" required>
+                    <br>
+                    <label for = "bio">Bio<span class = "req">*</span></label>
+                    <br>
+                    <textarea name = "bio" id = "bio" class = "mtrl-field req-field textarea-field" required></textarea>
+                    <br><br>
+                    <input type = "reset" class = "mtrl-button-outline-dark" value = "Reset form">
+                    <input type = "submit" class = "mtrl-button-dark" value = "Register">
+                    <br><br>
+                </fieldset>
+            </form>
+        </div>
     </body>
 </html>
 EOL;
